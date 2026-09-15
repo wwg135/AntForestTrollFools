123c519c1e86999c83f245f674fe9982aca2ffff
wwg135 <105048516+wwg135@users.noreply.github.com>
2026-09-15 18:16:27 +0800

    Add files via upload

--- PortEntry.m
+++ PortEntry.m
@@ -1603,7 +1603,7 @@ static void installEarnEnergyCollector(id controller) {
 
     [self.view addSubview:grabber];
     UILabel *versionLabel = [[UILabel alloc] init];
-    versionLabel.text = @"当前版本：v3.3.1";
+    versionLabel.text = @"当前版本：v3.3.2";
     versionLabel.font = [UIFont systemFontOfSize:11 weight:UIFontWeightRegular];
     versionLabel.textColor = [UIColor systemGray2Color];
     versionLabel.textAlignment = NSTextAlignmentCenter;
